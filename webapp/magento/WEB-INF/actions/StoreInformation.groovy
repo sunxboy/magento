7ccd2cd99d314b79c11371b065b8040fbb165829
Ravi Lodhi <ravi.lodhi@hotwaxmedia.com>
2014-11-24 17:54:48 +0530

    [OFBIZUS-37] Formatting changes only.

--- webapp/magento/WEB-INF/actions/StoreInformation.groovy
+++ webapp/magento/WEB-INF/actions/StoreInformation.groovy
@@ -3,25 +3,25 @@ import org.ofbiz.party.party.PartyWorker;
 
 if (productStore) {
     partyId = productStore.payToPartyId;
-if (partyId) {
-    companyMap = [:];
-    groupName = PartyHelper.getPartyName(delegator, partyId, false);
-    companyMap.groupName = groupName;
+    if (partyId) {
+        companyMap = [:];
+        groupName = PartyHelper.getPartyName(delegator, partyId, false);
+        companyMap.groupName = groupName;
 
-    /*Get postal address of company*/
-    postalAddress = PartyWorker.findPartyLatestPostalAddress(partyId, delegator);
-    companyMap.postalAddress = postalAddress;
+        /*Get postal address of company*/
+        postalAddress = PartyWorker.findPartyLatestPostalAddress(partyId, delegator);
+        companyMap.postalAddress = postalAddress;
 
-    /*Get email address of company*/
-    companyEmail = dispatcher.runSync("getPartyEmail", ['partyId': partyId, 'userLogin': parameters.userLogin]);
-    companyMap.companyEmail = companyEmail;
+        /*Get email address of company*/
+        companyEmail = dispatcher.runSync("getPartyEmail", ['partyId': partyId, 'userLogin': parameters.userLogin]);
+        companyMap.companyEmail = companyEmail;
 
-    /*Get contact number of company*/
-    telecomNumber = PartyWorker.findPartyLatestTelecomNumber(partyId, delegator);
-    if(telecomNumber) {
-        companyMap.telecomNumber= telecomNumber;
+        /*Get contact number of company*/
+        telecomNumber = PartyWorker.findPartyLatestTelecomNumber(partyId, delegator);
+        if (telecomNumber) {
+            companyMap.telecomNumber= telecomNumber;
+        }
+        context.companyMap = companyMap;
+        context.partyId = partyId;
     }
-    context.companyMap = companyMap;
-    context.partyId = partyId;
-}
 }
